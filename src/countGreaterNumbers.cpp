565dc9b2c54c3c8a84ef2d7b35e5b3d2fd603746
tech1arb <tech1arb@missionrnd.com>
2016-01-22 15:34:21 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -14,12 +14,56 @@ ERROR CASES: Return NULL for invalid inputs.
 NOTES:
 */
 
-struct transaction {
+struct transaction
+{
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date)
+{
+	int date1, month, year, count = 0, count1 = 0;
+	year = ((date[6] - 48) * 1000) + ((date[7] - 48) * 100) + ((date[8] - 48) * 10) + date[9] - 48;
+	month = (date[3] - 48) * 10 + date[4] - 48;
+	date1 = (date[0] - 48) * 10 + date[1] - 48;
+	//printf("%d\n",date1);
+	for (int i = len; i >= 0; i--)
+	{
+		int year1, month1, date2;
+		Arr[i].date;
+		year1 = ((Arr[i].date[6] - 48) * 1000) + ((Arr[i].date[7] - 48) * 100) + ((Arr[i].date[8] - 48) * 10) + Arr[i].date[9] - 48;
+		month1 = (Arr[i].date[3] - 48) * 10 + Arr[i].date[4] - 48;
+		date2 = (Arr[i].date[0] - 48) * 10 + Arr[i].date[1] - 48;
+		//printf("%d/n",date2);
+		if (year1 == year)
+		{
+			if (month1 == month)
+			{
+				if (date1 == date2)
+					break;
+				else
+				{
+					count++;
+					continue;
+				}
+			}
+			else
+			{
+				count++;
+				continue;
+			}
+		}
+		else
+		{
+			count++;
+			continue;
+		}
+	}
+	count1 = count - 1;
+	if (count1 == 5)
+		return 0;
+	else
+		return count1;
+	//printf("%d", count1);
 }
